72d8b619c34fb1867e836ba690fc44002b2f0658
Anatole VOLTZ <anatole.voltz@etu.unistra.fr>
2026-06-23 17:14:08 +0200

    mise à jour des polices dans le thème et suppression des liens de polices inutilisés

--- app/root.tsx
+++ app/root.tsx
@@ -11,16 +11,6 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700;900&family=Inter:wght@400;500;600;700;800&display=swap",
-  },
 ];
 
 type LayoutProps = { readonly children: React.ReactNode };
